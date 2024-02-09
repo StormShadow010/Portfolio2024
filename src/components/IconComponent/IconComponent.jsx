a98f40c2aacee71c44d8ee8eecd2ae1f8da6f6fc
Santiago <asjimenezg1923@gmail.com>
2024-02-08 23:08:55 -0500

    CambiosSkillsAll

--- src/components/IconComponent/IconComponent.jsx
+++ src/components/IconComponent/IconComponent.jsx
@@ -11,27 +11,36 @@ export const IconComponent = ({ icons }) => {
     const handleLeave = () => { setHoveredIcon(null); }
 
     const IndividualIcon = ({ icon, index, onHover }) => {
-        const paths = icon.icon()
+        const paths = icon.icon();
         const clonedPaths = React.Children.map(paths.props.children, (path) => {
-            return <path {...path.props} fill={onHover ? path.props.fill : (path.props['data-focus'] ? '#20264d' : 'white')} />;
-        })
+            return <path.type {...path.props} fill={onHover ? path.props.fill : (path.props['data-focus'] ? '#20264d' : 'white')} />;
+        });
 
         return (
-            <div key={index} className='w-[70px] h-[70px]'>
-                <div onMouseOver={() => handleHover(index)} onMouseLeave={handleLeave}>
-                    <svg viewBox="0 0 32 32">{clonedPaths}</svg>
+            <div
+                className='w-[85px] h-[70px] flex justify-center items-center flex-col mt-3 hover:scale-125 group'
+                onMouseOver={() => handleHover(index)}
+                onMouseLeave={handleLeave}
+            >
+                <div >
+                    <svg viewBox='0 0 32 32' width='100%' height='100%'>
+                        {clonedPaths}
+                    </svg>
+                </div>
+                <div>
+                    <span className='group-hover:text-[0.9rem] text-[0.8rem] font-mainFont font-bold text-black'>{icon.name}</span>
                 </div>
             </div>
-        )
-    }
+        );
+    };
 
     return (
-        <div className='flex'>
+        <div className='flex flex-wrap justify-center items-center gap-8 mt-2 md:h-[280px]'>
             {
                 icons.map((icon, index) => {
-                    return <IndividualIcon key={index} icon={icon} index={index} onHover={hoveredIcon === index} />;
+                    return <IndividualIcon key={index} icon={icon} index={index} onHover={hoveredIcon === index} />
                 })
             }
-        </div>
+        </div >
     )
 }
